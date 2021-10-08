eea8d2b0ed88a4397c6d32a054acf41cf899f4eb
mariavaghani <maria.vaghani@gmail.com>
2021-10-08 11:46:04 -0400

    Update webpack setup, add babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+  let canvas = document.getElementById('game-canvas');
+  let ctx = canvas.getContext('2d');
+
+  console.log("All Loaded, thank you for asking");
+})
